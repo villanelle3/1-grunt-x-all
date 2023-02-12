445f2cdd0b8114d38fd182a55210be7d8756cc5a
Laura Augusta <laurah.algusta@gmail.com>
2023-02-12 15:50:10 -0300

    new updates.

--- scripts/js.js
+++ scripts/js.js
@@ -3,3 +3,8 @@ console.log("arquivo2");
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 });
+
+
+$('.btn-menu').click(function () {
+    $(this).toggleClass('active');
+});
