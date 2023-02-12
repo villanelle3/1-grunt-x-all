99c65a84261c3ef5701be554e1be969889745e01
Laura Augusta <laurah.algusta@gmail.com>
2023-02-12 13:09:26 -0300

    grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,118 +1,55 @@
-module.exports = function (grunt) {
-
+module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
 
-        less: {
-            development: {
-                files: {
-                    'dev/styles/main.css': 'src/styles/main.less'
-                }
-            },
-            production: {
-                options: {
-                    compress: true,
-                },
-                files: {
-                    'dist/styles/main.min.css': 'src/styles/main.less'
-                }
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: './assets/sass',
+                    src: ['*.scss'],
+                    dest: './css',
+                    ext: '.css'
+                }]
             }
         },
 
         watch: {
-            less: {
-                files: ['src/styles/**/*.less'],
-                tasks: ['less:development']
-            },
-            html: {
-                files: ['src/index.html'],
-                tasks: ['replace:dev']
+            css: {
+                files: './assets/sass/*.scss',
+                tasks: ['sass', 'cssmin'],
             }
         },
 
-        replace: {
-            dev: {
-                options: {
-                    patterns: [
-                        {
-                            match: 'ENDERECO_DO_CSS',
-                            replacement: './styles/main.css'
-                        },
-                        {
-                            match: 'ENDERECO_DO_JS',
-                            replacement: '../src/scripts/main.js'
-                        }
-                    ]
-                },
-                files: [
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['src/index.html'],
-                        dest: 'dev/'
-                    }
-                ]
-            },
-            dist: {
-                options: {
-                    patterns: [
-                        {
-                            match: 'ENDERECO_DO_CSS',
-                            replacement: './styles/main.min.css'
-                        },
-                        {
-                            match: 'ENDERECO_DO_JS',
-                            replacement: './scripts/main.min.js'
-                        }
-                    ]
-                },
-
-                files: [
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['prebuild/index.html'],
-                        dest: 'dist/'
-                    }
-                ]
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: './css',
+                    src: ['*.css', '!*.min.css'],
+                    dest: './css',
+                    ext: '.min.css'
+                }]
             }
         },
-
-        htmlmin: {
-            dist: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: {
-                    'prebuild/index.html': 'src/index.html'
-                }
-            }
-        },
-
-        clean: ['prebuild'],
 
         uglify: {
-            target: {
+            options: {
+                mangle: false
+            },
+            my_target: {
                 files: {
-                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                    './scripts/*.min.js' : ['./scripts/*.js']
                 }
             }
         }
-
-
+        
     });
 
-
-
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-replace');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+    // load tasks
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean','uglify']);
-
-}
+    grunt.registerTask('default', 'watch');
+};
