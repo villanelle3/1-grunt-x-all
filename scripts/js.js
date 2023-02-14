1910c651ad957d2e0cb506bf84371147d8cb8ac4
Laura Augusta <laurah.algusta@gmail.com>
2023-02-14 17:14:12 -0300

    new updates.

--- scripts/js.js
+++ scripts/js.js
@@ -1,10 +1,21 @@
-console.log("arquivo2");
-
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 });
 
+$(document).ready(function(){
+    $(window).resize(function(){
+        var width = $(window).width();
+        if(width <= 959 && width >= 768 ){
+            $('.item').removeClass('col-md-4').addClass('col-md-6');
+        }
+        else{
+            $('.item').removeClass('col-md-6').addClass('col-md-4');
+        }
+    });
 
-$('.btn-menu').click(function () {
-    $(this).toggleClass('active');
-});
+    $('.btn-menu').click(function () {
+        $(this).toggleClass('active');
+        $("#nav-container").slideToggle();
+    });
+
+})
